36a86758db391ad34f9f522e368f6a089eac2d37
Pedro Lima Reis <pedrolimareis11@hotmail.com>
2023-12-09 19:39:36 -0300

    config gitignore

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Documentação')
+    .setDescription('Documentação da API')
     .setVersion('1.0')
     .addTag('cats')
     .build();
